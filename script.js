d889a741752717c20293a68dc746942a4c31fae5
rehan07-ops <joshfogi5005@gmail.com>
2026-06-26 00:50:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-    alert("Welcome to Rehan Connect!");
-});
+document.querySelector(".video").onclick = function () {
+    window.location.href = "index1.html";
+};
+
+document.querySelector(".date").onclick = function () {
+    alert("Dating feature coming soon!");
+};
+
+document.querySelector(".nearby").onclick = function () {
+    alert("Nearby Match coming soon!");
+};
+
+document.querySelector(".login").onclick = function () {
+    alert("Login page coming soon!");
+
