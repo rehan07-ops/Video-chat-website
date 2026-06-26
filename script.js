601327bb1937d279cfe73ee292cecbf904b6e37d
rehan07-ops <joshfogi5005@gmail.com>
2026-06-26 05:57:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-alert("Script Loaded");
-document.querySelector(".video").onclick = function () {
-    window.location.href = "index1.html";
-};
-
-document.querySelector(".date").onclick = function () {
-    alert("Dating feature coming soon!");
-};
-
-document.querySelector(".nearby").onclick = function () {
-    alert("Nearby Match coming soon!");
-};
-
-document.querySelector(".login").onclick = function () {
-    alert("Login page coming soon!");
-};
+a
 
